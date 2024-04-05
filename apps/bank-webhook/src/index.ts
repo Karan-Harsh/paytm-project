07b297551db9f6b1229eb0f25cacf38a209fd190
Harsh Karan <karanharshe577@gmail.com>
2024-04-05 22:22:17 +0530

    update balance

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,11 +1,33 @@
 import express from "express";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/hdfcbankwebhook", (req, res) => {
+app.post("/hdfcbankwebhook", async (req, res) => {
   const paymentInformation = {
     token: req.body.token,
     userId: req.body.user_Identifier,
     amount: req.body.amount,
   };
+  await db.balance.update({
+    where: {
+      userId: paymentInformation.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentInformation.amount,
+      },
+    },
+  });
+  db.onRampTransaction.update({
+    where: {
+      token: paymentInformation.token,
+    },
+    data: {
+      status: "Success",
+    },
+  });
+  res.json({
+    message: "Captured",
+  });
 });
